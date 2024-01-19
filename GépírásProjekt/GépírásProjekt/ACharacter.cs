a06c092874ad1c6cd512a51b068bf294d5771aab
polyakale <polyakalex52@gmail.hu>
2024-01-19 10:29:39 +0100

    commit

---
+++ GépírásProjekt/GépírásProjekt/ACharacter.cs
@@ -0,0 +1,16 @@
+﻿namespace GépírásProjekt
+{
+    internal class ACharacter
+    {
+        public char character;
+        public int pressingFinger;
+        public int additionalPressingFinger;
+
+        public ACharacter(char character, int pressingFinger, int additionalPressingFinger)
+        {
+            this.character = character;
+            this.pressingFinger = pressingFinger;
+            this.additionalPressingFinger = additionalPressingFinger;
+        }
+    }
+}
---
+++ GépírásProjekt/GépírásProjekt/Gépírás.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GépírásProjekt
+{
+    internal class Gépírás
+    {
+        private string file;
+        private string charactherFile;
+        List<ACharacter> characters = new List<ACharacter>();
+
+        public Gépírás(string file, string charactherFile)
+        {
+            this.file = file;
+            this.charactherFile = charactherFile;
+            ReadIn();
+        }
+
+        private void ReadIn()
+        {
+            string[] rows = File.ReadAllLines(charactherFile);
+            foreach (string row in rows.Skip(1))
+            {
+                string[] column = row.Split(';');
+                char character = char.Parse(column[0]);
+                int pressingFinger = int.Parse(column[1]);
+                int additionalPressingFinger = int.Parse(column[2]);
+                characters.Add(new ACharacter(character, pressingFinger, additionalPressingFinger));
+            }
+        }
+
+    }
+}
