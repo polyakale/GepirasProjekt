23b84db3874d0ee19c4a05094cd0ec4d6774b8ff
polyakale <polyakalex52@gmail.hu>
2024-01-31 11:13:43 +0100

    changes

--- Code/GépírásProjekt/GépírásProjekt/Program.cs
+++ Code/GépírásProjekt/GépírásProjekt/Program.cs
@@ -11,13 +11,8 @@ namespace GépírásProjekt
     {
         static void Main(string[] args)
         {
-            ////Example: go through the text
-            //string exampleText = "BafgaaAnobwpeFDSrargfva";
-            //for (int i = 0; i < exampleText.Length; i++) {
-            //    Console.WriteLine(exampleText[i]);
-            //}
+            //Check if the "szoveg.txt" file exists
             string file = "szoveg.txt";
-            //string path = @"\1. FÉLÉV\PROJEKT\GepirasProjekt\GépírásProjekt\GépírásProjekt\bin\Debug\szoveg.txt"; // not needed
             if (!File.Exists(file))
             {
                 Console.WriteLine($"The file '{file}' does not exist.");
